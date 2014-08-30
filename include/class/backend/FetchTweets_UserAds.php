0fb0c9b3e678023654e9c873cca0bdd93ff24912
Michael Uno <michael@michaeluno.jp>
2014-08-31 08:39:30 +0900

    Moved classes.

--- include/class/backend/FetchTweets_UserAds.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @package     Fetch Tweets
- * @copyright   Copyright (c) 2013, Michael Uno
- * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
- * @since		1.0.0
- * @description	Creates links for the user.
-*/
-final class FetchTweets_UserAds extends FetchTweets_UserAds_ {}
