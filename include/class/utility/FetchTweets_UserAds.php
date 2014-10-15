73658c9d2d147b533543e04752100c0e84d71b7b
Michael Uno <michael@michaeluno.jp>
2014-10-15 21:31:37 +0900

    Maybe moved the files.

---
+++ include/class/utility/FetchTweets_UserAds.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package     Fetch Tweets
+ * @copyright   Copyright (c) 2013, Michael Uno
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since		1.0.0
+*/
+/**
+ * Creates links for the user.
+ * 
+ */
+final class FetchTweets_UserAds extends FetchTweets_UserAds_ {}
