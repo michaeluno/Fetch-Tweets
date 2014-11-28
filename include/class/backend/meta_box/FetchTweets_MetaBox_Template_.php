1116855129002fefbd9e9c3e0855b6d4f937f496
Michael Uno <michael@michaeluno.jp>
2014-11-28 18:52:18 +0900

    Fixed code formatting for readability.

--- include/class/backend/meta_box/FetchTweets_MetaBox_Template_.php
+++ include/class/backend/meta_box/FetchTweets_MetaBox_Template_.php
@@ -6,14 +6,14 @@ abstract class FetchTweets_MetaBox_Template_ extends FetchTweets_AdminPageFramew
         $_oTemplates = FetchTweets_Templates::getInstance();
         $this->addSettingFields(            
             array(
-                'field_id'        => 'fetch_tweets_template',
-                'title'            => __( 'Select Template', 'fetch-tweets' ),
-                'description'    => __( 'Set the default template for this rule. If a template is specified in a widget, the shortcode, or the function, this setting will be overridden.', 'fetch-tweets' ),
-                'label'            => $arr = $_oTemplates->getTemplateArrayForSelectLabel(),
-                'type'            => 'select',
-                // 'after_field'     => '<pre>' . print_r( $arr, true ) . '</pre>', // debug
-                'default'            => $_oTemplates->getDefaultTemplateSlug(),
-                'show_title_column'    =>    false,
+                'field_id'          => 'fetch_tweets_template',
+                'title'             => __( 'Select Template', 'fetch-tweets' ),
+                'description'       => __( 'Set the default template for this rule. If a template is specified in a widget, the shortcode, or the function, this setting will be overridden.', 'fetch-tweets' ),
+                'label'             => $arr = $_oTemplates->getTemplateArrayForSelectLabel(),
+                'type'              => 'select',
+                // 'after_field'    => '<pre>' . print_r( $arr, true ) . '</pre>', // debug
+                'default'           => $_oTemplates->getDefaultTemplateSlug(),
+                'show_title_column' =>    false,
             ),                            
             array()
         );
