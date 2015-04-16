c37eaffc537d485195219d9283dacbd63f270d92
Michael Uno <michael@michaeluno.jp>
2015-04-16 17:05:55 +0900

    - Made the q parameter value being forced to be the string type. - Cleaned some typoes in doc-blocks.

--- include/class/request/fetch/FetchTweets_Fetch_BySearch.php
+++ include/class/request/fetch/FetchTweets_Fetch_BySearch.php
@@ -15,19 +15,19 @@ abstract class FetchTweets_Fetch_BySearch extends FetchTweets_Fetch_ByTag {
      * @see         https://dev.twitter.com/docs/api/1.1/get/search/tweets
      * @ramark      This request type does not support the 'include_rts' option. The Twitter API does not support it.
      * @param       string      $sKeyword
-     * @param       integer     $)deprecated        Deprecated. As of v1.3.4, request will be performed with the maximum count so that the caches will be reused for ones with lesser counts.
+     * @param       integer     $_deprecated        Deprecated. As of v1.3.4, request will be performed with the maximum count so that the caches will be reused for ones with lesser counts.
      * @param       string      $sLang              the language slug.  Default `en`
      * @param       string      $sUntil     
      * @param       string      $sGeoCode
      * @param       integer     $iCacheDuration     Default: `600`
      */ 
     protected function getTweetsBySearch( $sKeyword, $_deprecated=null, $sLang='en', $sResultType='mixed', $sUntil='', $sGeoCode='', $iCacheDuration=600 ) {
-         
+
         $_sRequestURI = add_query_arg(
             array_filter( 
                 array(
-                    'q'                 => urlencode_deep( trim( $sKeyword ) ),
-                    'result_type'       => $sResultType,    // 'mixed', 'recent', or 'popular'
+                    'q'                 => ( string ) urlencode_deep( trim( $sKeyword ) ),
+                    'result_type'       => $sResultType,    // 'mixed', 'recent', or 'popular'                    
                     'count'             => 100, // set the maximum
                     'lang'              => 'none'  === $sLang   ? '' : $sLang,
                     'until'             => empty( $sUntil )     ? '' : $sUntil,
