d869bb435b80727157c53535557e549e48aae926
Michael Uno <michael@michaeluno.jp>
2016-11-12 14:31:59 +0900

    Cleaned code.

--- include/class/shortcode/FetchTweets_Shortcode.php
+++ include/class/shortcode/FetchTweets_Shortcode.php
@@ -1,2 +1,30 @@
 <?php
-final class FetchTweets_Shortcode extends FetchTweets_Shortcode_ {}
+/**
+ * Fetch Tweets
+ * 
+ * Fetches and displays tweets from twitter.com.
+ * 
+ * http://en.michaeluno.jp/fetch-tweets/
+ * Copyright (c) 2013-2016 Michael Uno; Licensed GPLv2
+ */
+
+/**
+ * Handles the shortcode.
+ */
+class FetchTweets_Shortcode {
+    
+    /**
+     * Registers the shortcode.
+     */
+    public function __construct( $sShortCode ) {
+        add_shortcode( $sShortCode, array( $this, '_replyToGetOutput' ) );
+    }
+    
+    /**
+     * Returns the output by the given argument.
+     */
+    public function _replyToGetOutput( $aArgs ) {
+        return fetchTweets( $aArgs, false );
+    }    
+
+}
