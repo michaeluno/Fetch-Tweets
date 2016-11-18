a65cec01472324870993b098d1cd7d978ddecea2
Michael Uno <michael@michaeluno.jp>
2016-11-18 13:05:12 +0900

    Introduced the own http request handler in the `discover()` method to cache responses.

--- include/class/utility/FetchTweets_oEmbed.php
+++ include/class/utility/FetchTweets_oEmbed.php
@@ -14,7 +14,7 @@ class FetchTweets_oEmbed extends WP_oEmbed {
         parent::__construct();
         
         // Apply a fix for recent Instagram's image url format change.
-        add_filter( 'oembed_result', array( $this, 'sanitizeOEmbedResult' ), 10, 3 );
+        add_filter( 'oembed_result', array( __CLASS__, 'replyToSanitizeOEmbedResult' ), 10, 3 );
         
     }
     
@@ -31,8 +31,9 @@ class FetchTweets_oEmbed extends WP_oEmbed {
      */
     public function get_html( $sURL, $aArguments=array() ) {
         
-        if ( isset( self::$___aCachedURLs[ $sURL ] ) )
+        if ( isset( self::$___aCachedURLs[ $sURL ] ) ) {            
             return self::$___aCachedURLs[ $sURL ];
+        }
             
         $sHTML = parent::get_html( $sURL, $aArguments );
         
@@ -44,20 +45,111 @@ class FetchTweets_oEmbed extends WP_oEmbed {
     }
         static private $___aCachedURLs = array();
 
+	/**
+	 * Attempts to discover link tags at the given URL for an oEmbed provider.
+     * 
+	 * @param string $url The URL that should be inspected for discovery `<link>` tags.
+	 * @return false|string False on failure, otherwise the oEmbed provider URL.
+     * @remark      Overriding this method to use a custom HTTP client.
+	 */
+	public function discover( $url ) {
+		$providers = array();
+		$args = array(
+			'limit_response_size' => 153600, // 150 KB
+		);
+
+		/**
+		 * Filters oEmbed remote get arguments.
+		 *
+		 * @since 4.0.0
+		 *
+		 * @see WP_Http::request()
+		 *
+		 * @param array  $args oEmbed remote get arguments.
+		 * @param string $url  URL to be inspected.
+		 */
+		$args = apply_filters( 'oembed_remote_get_args', $args, $url );
+
+		// Fetch URL content
+        // Use a custom HTTP client.
+        $_oHTTP     = new FetchTweets_HTTP_Get( 
+            $url,
+            60 * 60 * 24 * 7,   // 1 week cache duration
+            $args
+        );    
+        $_oHTTP->setType( 'oembed_get' ); // mark the request type
+		$request = $_oHTTP->get();
+        
+		if ( $html = wp_remote_retrieve_body( $request ) ) {
+
+			/**
+			 * Filters the link types that contain oEmbed provider URLs.
+			 *
+			 * @since 2.9.0
+			 *
+			 * @param array $format Array of oEmbed link types. Accepts 'application/json+oembed',
+			 *                      'text/xml+oembed', and 'application/xml+oembed' (incorrect,
+			 *                      used by at least Vimeo).
+			 */
+			$linktypes = apply_filters( 'oembed_linktypes', array(
+				'application/json+oembed' => 'json',
+				'text/xml+oembed' => 'xml',
+				'application/xml+oembed' => 'xml',
+			) );
+
+			// Strip <body>
+			if ( $html_head_end = stripos( $html, '</head>' ) ) {
+				$html = substr( $html, 0, $html_head_end );
+			}
+
+			// Do a quick check
+			$tagfound = false;
+			foreach ( $linktypes as $linktype => $format ) {
+				if ( stripos($html, $linktype) ) {
+					$tagfound = true;
+					break;
+				}
+			}
+
+			if ( $tagfound && preg_match_all( '#<link([^<>]+)/?>#iU', $html, $links ) ) {
+				foreach ( $links[1] as $link ) {
+					$atts = shortcode_parse_atts( $link );
+
+					if ( !empty($atts['type']) && !empty($linktypes[$atts['type']]) && !empty($atts['href']) ) {
+						$providers[$linktypes[$atts['type']]] = htmlspecialchars_decode( $atts['href'] );
+
+						// Stop here if it's JSON (that's all we need)
+						if ( 'json' == $linktypes[$atts['type']] )
+							break;
+					}
+				}
+			}
+		}
+
+		// JSON is preferred to XML
+		if ( !empty($providers['json']) )
+			return $providers['json'];
+		elseif ( !empty($providers['xml']) )
+			return $providers['xml'];
+		else
+			return false;
+	}        
+        
     
     /**
      * Sanitizes oEmebed results.
      * 
-     * @remark            Currently only Instagram's images will be checked.
-     * @since            1.3.3.1
+     * @remark          Currently only Instagram's images will be checked.
+     * @since           1.3.3.1
+     * @since           2.5.0       Changed the scope to static.
      */
-    public function sanitizeOEmbedResult( $sHTML, $sURL, $aArgs ) {
+    static public function replyToSanitizeOEmbedResult( $sHTML, $sURL, $aArgs ) {
         
         // A fix for Instagram - src="http://distilleryimage7.ak.instagram.com/0a0e19404b4a11e38add1240bd2c384e_7.jpg  ---> <img src="http://distilleryimage7.ak.instagram.com/0a0e19404b4a11e38add1240bd2c384e_8.jpg
         if ( preg_match( "/:\/\/instagr/", $sURL ) ) {            
             $sHTML = preg_replace_callback( 
                 '/(\ssrc=(["\']))(.+_)(\d)(\..+?)(\2)/i',   // '
-                array( $this, '___getInstagramImageURLReplaceed' ), 
+                array( __CLASS__, '___getInstagramImageURLReplaceed' ), 
                 $sHTML 
             );
         }  
@@ -65,12 +157,12 @@ class FetchTweets_oEmbed extends WP_oEmbed {
         
     }
         /**
-         * A helper function for the above sanitizeOEmbedResult() method.
+         * A helper function for the above replyToSanitizeOEmbedResult() method.
          * 
          * @since       1.3.3.2
          * @return      string
          */
-        private function ___getInstagramImageURLReplaceed( $aMatches ) {
+        static private function ___getInstagramImageURLReplaceed( $aMatches ) {
             
             if ( count( $aMatches ) != 7 ) {
                 return $aMatches[ 0 ];
