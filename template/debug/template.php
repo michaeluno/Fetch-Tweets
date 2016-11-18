d89cebc5f5c3927b6406d400fcfac2a02092034f
Michael Uno <michael@michaeluno.jp>
2016-11-18 18:02:08 +0900

    Fixed code formatting.

--- template/debug/template.php
+++ template/debug/template.php
@@ -1,11 +1,10 @@
 <?php
-/*
+/**
  * Available variables passed from the caller script
- * - $aTweets : the fetched tweet arrays.
- * - $aArgs	: the passed arguments such as item count etc.
+ * - $aTweets  : the fetched tweet arrays.
+ * - $aArgs	   : the passed arguments such as item count etc.
  * - $aOptions : the plugin options saved in the database.
- * */
- 
+ */
  ?>
 <div class='var_dump'>
 	<?php var_dump( $aTweets ); ?>
