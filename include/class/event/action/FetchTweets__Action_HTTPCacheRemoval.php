b15ab3f9a7b167b7b3b090984d7b6825aeaa0fb6
Michael Uno <michael@michaeluno.jp>
2016-11-20 01:39:50 +0900

    Refactored a method.

--- include/class/event/action/FetchTweets__Action_HTTPCacheRemoval.php
+++ include/class/event/action/FetchTweets__Action_HTTPCacheRemoval.php
@@ -21,39 +21,14 @@ class FetchTweets__Action_HTTPCacheRemoval extends FetchTweets__Action_Base {
 
     protected function _construct() {
          
-        $this->___scheduleBackgroundCacheRemoval();
-         
+        $this->scheduleWPCronActionOnce( 
+            $this->_sActionName, 
+            array(), 
+            FetchTweets_Option::get( array( 'cache_settings', 'clearing_interval', 'size' ), 7 )
+            * FetchTweets_Option::get( array( 'cache_settings', 'clearing_interval', 'unit' ), 86400 )
+        );
+        
     }
-        /**
-         * 
-         * @return      boolean     True if scheduled. Otherwise, false.
-         */
-        private function ___scheduleBackgroundCacheRemoval() {
-                
-            $_aArguments = array();
-            if ( wp_next_scheduled( $this->_sActionName, $_aArguments ) ) {
-                return false; 
-            }
-            
-            $_oOption   = FetchTweets_Option::getInstance();
-            $_aInterval = $_oOption->get( 
-                array( 'cache_settings', 'clearing_inteval' ),
-                array(
-                    'size'     => 7,
-                    'unit'     => 86400,            
-                )
-            );            
-            $_iInterval = ( integer ) $this->getElement( $_aInterval, 'size' )
-                * ( integer ) $this->getElement( $_aInterval, 'unit' );
-            $_bCancelled = wp_schedule_single_event( 
-                time() + $_iInterval, 
-                $this->_sActionName, // the FetchTweets_Event class will check this action hook and executes it with WP Cron.
-                $_aArguments // must be enclosed in an array.
-            );          
-            return false !== $_bCancelled;
-            
-        }
-
     
     /**
      * Deletes expired caches.
