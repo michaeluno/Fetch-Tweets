8e20cd5937774d347e4710cf07350d87d13e8f7e
Michael Uno <michael@michaeluno.jp>
2016-12-22 00:19:21 +0900

    Made it display rule arguments.

--- template/debug/readme.txt
+++ template/debug/readme.txt
@@ -1,7 +1,10 @@
 == Change log ==
 
+= 1.1.0 =
+- Made it display rule arguments as well.
+
 = 1.0.1 =
-Tweaked the styling. 
+- Tweaked the styling.
 
 = 1.0.0 =
-Initial release.
+- Initial release.
--- template/debug/template.php
+++ template/debug/template.php
@@ -1,11 +1,16 @@
 <?php
 /**
  * Available variables passed from the caller script
- * - $aTweets  : the fetched tweet arrays.
- * - $aArgs	   : the passed arguments such as item count etc.
- * - $aOptions : the plugin options saved in the database.
+ * - $aTweets    : the fetched tweet arrays.
+ * - $aArguments : the passed arguments such as item count etc.
+ * - $aOptions   : the plugin options saved in the database.
  */
  ?>
+<h4>Arguments</h4>
+<div class='var_dump'>
+	<?php var_dump( $aArguments ); ?>
+</div>
+<h4>Tweets</h4>
 <div class='var_dump'>
 	<?php var_dump( $aTweets ); ?>
 </div>
